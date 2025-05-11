0a6e49f282840c1b4ee0d8cfaee760a878d5beb0
Loris <buzzes_pallets5s@icloud.com>
2025-05-11 12:13:05 +0200

    add firebase connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
-import { Button } from "@/components/ui/button";
+import { addTrail } from "@/lib/persistencyService";
+import { Trail } from "@/types/trails";
 
 export default function Home() {
+  const sampleTrail: Trail = {
+    id: "1",
+    name: "Mountain Adventure",
+    description: "A scenic trail through the mountains with breathtaking views.",
+    distance: 15, // in kilometers
+    durationMinute: 240, // 4 hours
+    elevationGainMeters: 800, // in meters
+    difficulty: "T3",
+    imageUrl: "https://placehold.co/600x400"
+  };
+  async function addSampleTrail() {
+    const result = await addTrail(sampleTrail);
+    if (result) {
+      console.log("Trail added successfully:", result);
+    } else {
+      console.error("Failed to add trail.");
+    }
+  }
+
+  addSampleTrail();
   return (
     <main>
       <h1>Hello World</h1>
-      <Button>Click Me</Button>
     </main>
   );
 }
--- src/lib/persistencyService.ts
+++ src/lib/persistencyService.ts
@@ -1,39 +1,13 @@
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "./firebaseConfig";
 import { Trail } from "@/types/trails";
 
-export function getTrails(): Trail[] {
-    const trails = localStorage.getItem('trails');
-    if (trails) {
-        return JSON.parse(trails);
+export async function addTrail(trail: Trail): Promise<{ id: string; data: Trail } | null> {
+    try {
+        const trailRef = await addDoc(collection(db, "trails"), trail);
+        return { id: trailRef.id, data: trail };
+    } catch (error) {
+        console.error("Error adding trail: ", error);
+        return null;
     }
-    return [];
-}
-
-export function getTrail(trailId: string): Trail | undefined {
-    const trails = getTrails();
-    return trails.find((trail: Trail) => trail.id === trailId);
-}
-
-export function saveTrails(trails: Trail[]) {
-    localStorage.setItem('trails', JSON.stringify(trails));
-}
-
-export function addTrail(trail: Trail) {
-    const trails = getTrails();
-    trails.push(trail);
-    saveTrails(trails);
-}
-
-export function updateTrail(updatedTrail: Trail) {
-    const trails = getTrails();
-    const trailIndex = trails.findIndex((trail: Trail) => trail.id === updatedTrail.id);
-    if (trailIndex !== -1) {
-        trails[trailIndex] = updatedTrail;
-        saveTrails(trails);
-    }
-}
-
-export function deleteTrail(trailId: string) {
-    const trails = getTrails();
-    const updatedTrails = trails.filter((trail: Trail) => trail.id !== trailId);
-    saveTrails(updatedTrails);
 }
